05c5616bdafe0f78bd5b02b1374968cbd6fe2be3
diego-vega <ferzux.09@gmail.com>
2024-10-04 17:13:12 -0500

    first commit into develop

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+    return <h1>Holaa todo app</h1>;
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+import * as path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    resolve: {
+        alias: [
+            { find: "@", replacement: path.resolve(import.meta.url, "src") },
+        ],
+    },
+});
